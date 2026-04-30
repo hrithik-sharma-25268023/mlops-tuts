88f2eb5594f8224d1fc0b7465535ec8827a0129d
hrithik-sharma25268023 <25268023@studentmail.ul.ie>
2026-04-30 23:36:22 +0100

    signin sign up created

--- oops/oops_proj.py
+++ oops/oops_proj.py
@@ -18,19 +18,45 @@ class ChatBook:
                            5. Press any other key to exit
                            """)
         if user_input == '1':
-            pass
+            self.sign_up()
+
         elif user_input == '2':
-            pass
+            self.sign_in()
+
         elif user_input == '3':
             pass
         elif user_input == '4':
             pass
         else:
-            exit()    
+            exit()
+
+        
+    def sign_up(self):
+        email = input("Enter your Email:- ")
+        password = input("Setup your Password:- ")
+        self.user_name = email
+        self.password = password
+        print("You have signed up successfully !!")
+        print()
+        self.menu()
+
+    def sign_in(self):
+        if self.user_name == '' and self.password == '':
+            print("Please Signup First by pressing 1 in main menu")
+        else:
+            uname = input("Enter your Email:- ")
+            pwd = input("enter your password here:")
+            
+            if self.user_name == uname and self.password == pwd:
+                print("Login Successful")
+                self.loggedin = True
+            else:
+                print("Please input correct credentials")
+        self.menu()
 
 if __name__ == "__main__":
     obj = ChatBook()
-        
+
 
     
     
